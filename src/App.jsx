2b4c93bcd2545149dfafa4bfbb2e6383cebd1728
Ken <agnan.jaisil@gmail.com>
2026-07-13 22:09:57 +0530

    ViewCars Complete

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import ViewCars from './components/ViewCars'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <ViewCars/>
       
     </>
   )
